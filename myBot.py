490a7309f59aab55c3e8bf27d749be1e3f9309e7
Imam Alfarizi Syahputra <63343105+ImamAlfariziSyahputra@users.noreply.github.com>
2020-06-12 09:05:00 +0700

    Menambah fitur command /datasiswa
    Pengoneksian ke database dan menambah command baru untuk menampilkan datasiswa dari database

--- myBot.py
+++ myBot.py
@@ -1,11 +1,15 @@
 import telebot
 import myToken
-
+import mysql.connector
 from datetime import date
 from datetime import datetime
 
 TOKEN=myToken.TOKEN
 myBot = telebot.TeleBot(TOKEN)
+
+myDb = mysql.connector.connect(host='localhost', user='root', password='', database='db_belajarbot')
+sql = myDb.cursor()
+
 from telebot import apihelper
 waktuSekarang = datetime.now()
 
@@ -18,5 +22,30 @@ class Mybot:
         teks = myToken.SAPA + "\n-- admin & dev @ImamAlfarizi --" + "\n" \
                             "hari ini tanggal " + str(waktuSekarang)
         myBot.reply_to(message, teks)
+
+    @myBot.message_handler(commands=['datasiswa'])
+    def menu_data_siswa(message):
+        query = "SELECT nipd,nama,kelas FROM table_siswa"
+        sql.execute(query)
+        data = sql.fetchall()
+        jmldata = sql.rowcount
+        kumpuldata=''
+
+        if(jmldata > 0):
+            #print(data)
+            no = 0
+            for x in data:
+                no += 1
+                kumpuldata = kumpuldata + str(x) + '\n'
+                print(kumpuldata)
+                kumpuldata = kumpuldata.replace('(','')
+                kumpuldata = kumpuldata.replace(')', '')
+                kumpuldata = kumpuldata.replace("'", '')
+                kumpuldata = kumpuldata.replace(',', '')
+        else:
+            print('datakosong')
+
+        myBot.reply_to(message, str(kumpuldata))
+
 print("-- Bot is Running --")
 myBot.polling(none_stop=True)
